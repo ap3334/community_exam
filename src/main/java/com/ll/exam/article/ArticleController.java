230a88bfc08aafe7a37bbbfd54935f94f444a075
ap3334 <chwil2989@naver.com>
2022-07-29 11:39:14 +0900

    게시물 작성, 수정, 삭제 후에 location.replace로 바로이동

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -34,7 +34,7 @@ public class ArticleController {
 
         long id = articleService.write(title, body);
 
-        rq.println("%d번 게시물이 생성 되었습니다.".formatted(id));
+        rq.replace("/usr/article/detail/free/%d".formatted(id), "%d번 게시물이 생성 되었습니다.".formatted(id));
     }
 
     public void showDetail(Rq rq) {
@@ -74,9 +74,7 @@ public class ArticleController {
 
         articleService.deleteById(id);
 
-        rq.println("<div>%d번 게시물이 삭제되었습니다.</div>".formatted(id));
-        rq.println("<div><a href=\"/usr/article/list\">리스트로 이동</a></div>");
-
+        rq.replace("/usr/article/list/free", "%d번 게시물이 삭제되었습니다.".formatted(id));
     }
 
     public void showModify(Rq rq) {
@@ -107,7 +105,6 @@ public class ArticleController {
 
         articleService.modify(id, title, body);
 
-        rq.println("<div>%d번 게시물이 수정되었습니다.</div>".formatted(id));
-        rq.println("<div><a href=\"/usr/article/detail/free/%d\">수정된 글로 이동</a></div>".formatted(id));
+        rq.replace("/usr/article/detail/free/%d".formatted(id), "%d번 게시물이 수정되었습니다.".formatted(id));
     }
 }
